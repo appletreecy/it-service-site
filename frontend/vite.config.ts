5cf37de5f72839773900d8d84271d7e4085d1ccb
appletreecy <appletreecy@gmail.com>
2026-01-22 14:31:44 +1100

    switch to tailwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 
@@ -9,5 +10,5 @@ export default defineConfig({
             "/api": "http://localhost:4000",
         },
     },
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
 });
